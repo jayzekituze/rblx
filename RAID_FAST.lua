b4f10d672811a044abdce294b9dc1bb4a5132145
jayzekituze <102671903+jayzekituze@users.noreply.github.com>
2025-03-28 12:30:37 +0800

    n346n346

--- RAID_FAST.lua
+++ RAID_FAST.lua
@@ -4,7 +4,7 @@ print("AccRaid_BETA")
 
 wait(5)
 
-getgenv().AutoRaidFAST = true -- to turn stop doing raids do "false" to start it again do "true"
+getgenv().AutoRaids = true -- to turn stop doing raids do "false" to start it again do "true"
 
 task.spawn(function()
     wait(5)
@@ -14,37 +14,51 @@ task.spawn(function()
         local character = player.Character or player.CharacterAdded:Wait()
         local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 
-
         local targetPosition = Vector3.new(-5285.70751953125, 161.93984985351562, -500.4004821777344)
-
 
         humanoidRootPart.CFrame = CFrame.new(targetPosition)
     end
-
 
     teleportPlayer()
 end)
 
 task.spawn(function()
-    while task.wait(0.1) do
-        if getgenv().AutoRaidFAST == true then
+    task.wait(10)
+    if getgenv().AutoRaids == true then
 
-            local raid = game.Workspace:FindFirstChild("raid")
-            local eternalDragon = raid and raid:FindFirstChild("eternal_dragon")
-            local blueEyes = eternalDragon and eternalDragon:FindFirstChild("Blue Eyes White Dragon")
-            local prox = blueEyes and blueEyes:FindFirstChild("ProximityPrompt")
+        local raid = game.Workspace:FindFirstChild("raid")
+        local eternalDragon = raid and raid:FindFirstChild("eternal_dragon")
+        local blueEyes = eternalDragon and eternalDragon:FindFirstChild("Blue Eyes White Dragon")
+        local prox = blueEyes and blueEyes:FindFirstChild("ProximityPrompt")
 
-            if prox then
-                fireproximityprompt(prox)
-                print("Raid Started")
-                task.wait(0.1)
-            else
-                print("Proximity Prompt not found!")
-            end
+        if prox then
+            fireproximityprompt(prox)
+            print("Raid Started")
+            task.wait(raidDoneCD)
+        else
+            print("Proximity Prompt not found!")
         end
     end
 end)
 
+
+
 task.spawn(function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/jayzekituze/rblx/refs/heads/main/AnTIAFK'))()
+    while task.wait(0.1) do
+        if getgenv().AutoRaids == true then
+            -- Check if the "battle-end" GUI is present
+            if game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("battle-end") then
+                local raid = game.Workspace:FindFirstChild("raid")
+                local eternalDragon = raid and raid:FindFirstChild("eternal_dragon")
+                local blueEyes = eternalDragon and eternalDragon:FindFirstChild("Blue Eyes White Dragon")
+                local prox = blueEyes and blueEyes:FindFirstChild("ProximityPrompt")
+
+                if prox then
+                    fireproximityprompt(prox)
+                else
+                    print("Proximity Prompt not found!")
+                end
+            end
+        end
+    end
 end)
